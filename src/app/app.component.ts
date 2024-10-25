e3153779efccf334e6e926e93d8b781ab6419334
Manuela Arroyo <manuela.arroyo@utp.edu.co>
2024-10-25 11:04:35 -0500

    Del padre al hijo, inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImgComponent } from "./components/img/img.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImgComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-store';
+  imgParent = '';
 }
---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-img',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './img.component.html',
+  styleUrl: './img.component.scss'
+})
+export class ImgComponent {
+  @Input() img: string = 'Valor Init';
+
+}
