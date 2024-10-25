eaae0bbfb411a42008991f52a4b8bcfc414e7dbb
Manuela Arroyo <manuela.arroyo@utp.edu.co>
2024-10-25 15:00:33 -0500

    side menu

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
 
+  activeMenu = false;
+
+  toogleMenu() {
+    this.activeMenu = !this.activeMenu;
+  }
 }
