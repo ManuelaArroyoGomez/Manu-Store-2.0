0b3b65a190cd6fd5b263681b48e9b4b65222c8e2
Manuela Arroyo <manuela.arroyo@utp.edu.co>
2024-10-25 16:55:09 -0500

    Pipes

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,17 +2,20 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../../models/product.model'
 import { CommonModule } from '@angular/common'
 
+import { TimeAgoPipe } from '../../pipe/time-ago.pipe';
 import { AppComponent } from './../../app.component'
 import { ImgComponent } from "../img/img.component";
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule, AppComponent, ImgComponent],
+  imports: [CommonModule, AppComponent, ImgComponent, TimeAgoPipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
+
+  today = new Date();
 
   @Input() product: Product = {
     id: '',
